eec4b9737057a98212e7ae0419bd2eec462c1473
faquinoortiz <144743802+faquinoortiz@users.noreply.github.com>
2023-09-27 07:24:17 -0700

    Project Update

--- interactive-form-v3/script.js
+++ interactive-form-v3/script.js
@@ -1,22 +1,16 @@
-
 const nameInput = document.getElementById("name");
 console.log(nameInput);
 nameInput.focus();
 
-const jobRoleSelect = document.getElementById("title"); 
-const otherJobRoleInput = document.getElementById("title");
-console.log(jobRoleSelect);
-console.log(otherJobRoleInput);
+const jobRoleSelect = document.getElementById("title");
+const otherJobRoleInput = document.getElementById("other-job-role");
 
-otherJobRoleInput.style.display = "none";
-
-jobRoleSelect.addEventListener("change",function(event){
-if (event.target.value === "other"){
+jobRoleSelect.addEventListener("change", function (event) {
+  if (event.target.value === "other") {
     otherJobRoleInput.style.display = "block";
-        }else{
+  } else {
     otherJobRoleInput.style.display = "none";
-
-
+  }
 });
 
 const designSelect = document.getElementById("design");
@@ -27,23 +21,66 @@ console.log(colorSelect);
 
 colorSelect.disabled = true;
 
-designSelect.addEventListener("change",function(event){
-    colorSelect.disabled = false;
+designSelect.addEventListener("change", function (event) {
+  colorSelect.disabled = false;
 
-    const selectedDesign = event.target.value;
+  const selectedDesign = event.target.value;
 
-
-for (let i = 0; i < colorOptions.length; i++){
+  for (let i = 0; i < colorOptions.length; i++) {
     const option = colorOptions[i];
     const optionTheme = option.getAttribute("data-theme");
-    
-    if(selectedDesign === optionTheme){
-        option.hidden = false;
-        option.selected = true;
-        } else {
-        option.hidden = true;
-        option.selected = false; 
-        }
-    }
 
+    if (selectedDesign === optionTheme) {
+      option.hidden = false;
+      option.selected = true;
+    } else {
+      option.hidden = true;
+      option.selected = false;
+    }
+  }
+});
+
+const activitiesFieldset = document.getElementById("activities");
+const totalCostSpan = document.getElementById("totalCost");
+let totalCost = 0;
+
+console.log(activitiesFieldset);
+console.log(totalCostSpan);
+activitiesFieldset.addEventListener("change", function (event) {
+  const dataCost = +event.target.getAttribute("data-cost");
+  const isChecked = event.target.checked;
+
+  if (isChecked) {
+    totalCost += dataCost;
+  } else {
+    totalCost -= dataCost;
+  }
+
+  totalCostSpan.textContent = "$" + totalCost;
+});
+
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+checkboxes.forEach((checkbox) => {
+  const label = checkbox.parentElement;
+  checkbox.addEventListener("focus", () => {
+    label.classList.add("focus");
+  });
+  checkbox.addEventListener("blur", () => {
+    label.classList.remove("focus");
+  });
+});
+
+const form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  const nameValid = /^[a-zA-Z\s]+$/.test(nameInput.value);
+  if (!nameValid) {
+    event.preventDefault();
+    nameInput.parentElement.classList.remove("valid");
+    nameInput.parentElement.classList.add("not-valid");
+    nameInput.parentElement.lastElementChild.style.display = "block";
+  } else {
+    nameInput.parentElement.classList.remove("not-valid");
+    nameInput.parentElement.classList.add("valid");
+    nameInput.parentElement.lastElementChild.style.display = "none";
+  }
 });
