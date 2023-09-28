29d590ae61333af94b28fbc1516718e449f4642f
faquinoortiz <144743802+faquinoortiz@users.noreply.github.com>
2023-09-28 14:02:32 -0700

    Updated credit card and errors

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,30 +1,47 @@
+//All DOM elements are listed below
 const nameInput = document.getElementById("name");
-console.log(nameInput);
-nameInput.focus();
-
+const emailInput = document.getElementById("email");
 const jobRoleSelect = document.getElementById("title");
 const otherJobRoleInput = document.getElementById("other-job-role");
+const activitiesFieldset = document.getElementById("activities");
+const paymentSelect = document.getElementById("payment");
+const creditCardDiv = document.getElementById("credit-card");
+const paypalDiv = document.getElementById("paypal");
+const bitcoinDiv = document.getElementById("bitcoin");
+const form = document.querySelector("form");
 
-jobRoleSelect.addEventListener("change", function (event) {
-  if (event.target.value === "other") {
-    otherJobRoleInput.style.display = "block";
-  } else {
-    otherJobRoleInput.style.display = "none";
+//Will allow for a display of an element
+function showElement(element) {
+  element.style.display = "block";
+}
+function hideElement(element) {
+  element.style.display = "none";
+}
+
+function setOtherJobRoleVisibility() {
+  otherJobRoleInput.style.display = jobRoleSelect.value === "other" ? "block" : "none";
+}
+//Conditions for the payment for the selected payment visability
+function setPaymentSectionVisibility(selectedPayment) {
+  showElement(creditCardDiv);
+  showElement(paypalDiv);
+  showElement(bitcoinDiv);
+
+  if (selectedPayment === "credit-card") {
+    hideElement(paypalDiv);
+    hideElement(bitcoinDiv);
+  } else if (selectedPayment === "paypal") {
+    hideElement(creditCardDiv);
+    hideElement(bitcoinDiv);
+  } else if (selectedPayment === "bitcoin") {
+    hideElement(creditCardDiv);
+    hideElement(paypalDiv);
   }
-});
+}
 
-const designSelect = document.getElementById("design");
-const colorSelect = document.getElementById("color");
-const colorOptions = colorSelect.children;
-console.log(designSelect);
-console.log(colorSelect);
-
-colorSelect.disabled = true;
-
-designSelect.addEventListener("change", function (event) {
-  colorSelect.disabled = false;
-
-  const selectedDesign = event.target.value;
+//Updates and gives color options based on the design that is selected
+function updateColorOptions() {
+  const selectedDesign = designSelect.value;
 
   for (let i = 0; i < colorOptions.length; i++) {
     const option = colorOptions[i];
@@ -38,39 +55,130 @@ designSelect.addEventListener("change", function (event) {
       option.selected = false;
     }
   }
+}
+
+const designSelect = document.getElementById("design");
+const colorSelect = document.getElementById("color");
+const colorOptions = colorSelect.children;
+
+setOtherJobRoleVisibility();
+setPaymentSectionVisibility(paymentSelect.value);
+updateColorOptions();
+
+jobRoleSelect.addEventListener("change", setOtherJobRoleVisibility);
+
+paymentSelect.addEventListener("change", () => {
+  setPaymentSectionVisibility(paymentSelect.value);
 });
 
-const activitiesFieldset = document.getElementById("activities");
-const totalCostSpan = document.getElementById("activities-cost");
+designSelect.addEventListener("change", updateColorOptions);
+
 let totalCost = 0;
 
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-checkboxes.forEach((checkbox) => {
-  checkbox.addEventListener("change", function (event) {
-    const dataCost = +event.target.getAttribute("data-cost");
-    const isChecked = event.target.checked;
+//Updates the cost everytime a different activity is selected
+function updateTotalCost() {
+  const checkboxes = activitiesFieldset.querySelectorAll("input[type='checkbox']");
+  totalCost = 0;
 
-    if (isChecked) {
-      totalCost += dataCost;
-    } else {
-      totalCost -= dataCost;
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      totalCost += parseInt(checkbox.getAttribute("data-cost"));
     }
+  });
 
-    totalCostSpan.textContent = `Total: $${totalCost}`;
+  totalCostDisplay.textContent = `Total: $${totalCost}`;
+}
+
+const totalCostDisplay = document.createElement("p");
+activitiesFieldset.appendChild(totalCostDisplay);
+
+activitiesFieldset.addEventListener("change", updateTotalCost);
+
+const ccNumInput = document.getElementById("cc-num");
+const zipInput = document.getElementById("zip");
+const cvvInput = document.getElementById("cvv");
+const ccHint = document.getElementById("cc-hint");
+const zipHint = document.getElementById("zip-hint");
+const cvvHint = document.getElementById("cvv-hint");
+
+
+function validateCreditCard() {
+  const ccNumValid = /^\d{13,16}$/.test(ccNumInput.value);
+  const zipValid = /^\d{5}$/.test(zipInput.value);
+  const cvvValid = /^\d{3}$/.test(cvvInput.value);
+
+  if (!ccNumValid) {
+    ccNumInput.parentElement.classList.add("not-valid");
+  } else {
+    ccNumInput.parentElement.classList.remove("not-valid");
+  }
+
+  if (!zipValid) {
+    zipInput.parentElement.classList.add("not-valid");
+  } else {
+    zipInput.parentElement.classList.remove("not-valid");
+  }
+
+  if (!cvvValid) {
+    cvvInput.parentElement.classList.add("not-valid");
+  } else {
+    cvvInput.parentElement.classList.remove("not-valid");
+  }
+
+  ccHint.style.display = ccNumValid ? "none" : "block";
+  zipHint.style.display = zipValid ? "none" : "block";
+  cvvHint.style.display = cvvValid ? "none" : "block";
+}
+
+ccNumInput.addEventListener("input", validateCreditCard);
+zipInput.addEventListener("input", validateCreditCard);
+cvvInput.addEventListener("input", validateCreditCard);
+
+paymentSelect.value = "credit-card";
+setPaymentSectionVisibility("credit-card");
+
+const checkboxes = activitiesFieldset.querySelectorAll("input[type='checkbox']");
+checkboxes.forEach((checkbox) => {
+  const label = checkbox.parentElement;
+  checkbox.addEventListener("focus", () => {
+    label.classList.add("focus");
+  });
+  checkbox.addEventListener("blur", () => {
+    label.classList.remove("focus");
   });
 });
 
-const form = document.querySelector("form");
+//Event Listener for submission
 form.addEventListener("submit", function (event) {
+  
   const nameValid = /^[a-zA-Z\s]+$/.test(nameInput.value);
   if (!nameValid) {
-    event.preventDefault();
-    nameInput.parentElement.classList.remove("valid");
     nameInput.parentElement.classList.add("not-valid");
-    nameInput.parentElement.lastElementChild.style.display = "block";
   } else {
     nameInput.parentElement.classList.remove("not-valid");
-    nameInput.parentElement.classList.add("valid");
-    nameInput.parentElement.lastElementChild.style.display = "none";
+  }
+
+  const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput.value);
+  if (!emailValid) {
+    emailInput.parentElement.classList.add("not-valid");
+  } else {
+    emailInput.parentElement.classList.remove("not-valid");
+  }
+
+  
+  const checkboxes = activitiesFieldset.querySelectorAll("input[type='checkbox']");
+  const activitiesValid = Array.from(checkboxes).some((checkbox) => checkbox.checked);
+  if (!activitiesValid) {
+    activitiesFieldset.classList.add("not-valid");
+  } else {
+    activitiesFieldset.classList.remove("not-valid");
+  }
+  if (paymentSelect.value === "credit-card") {
+    validateCreditCard();
+  }
+  if (!nameValid || !emailValid || !activitiesValid || paymentSelect.value === "select method") {
+    event.preventDefault();
   }
 });
+
+nameInput.focus();
